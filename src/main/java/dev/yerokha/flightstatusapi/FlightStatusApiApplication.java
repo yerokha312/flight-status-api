c2070a50cf2084c7c883c6959b597416ff16f98f
yerbolat <erbolatt@live.com>
2024-06-26 17:11:14 +0500

    Initial commit

---
+++ src/main/java/dev/yerokha/flightstatusapi/FlightStatusApiApplication.java
@@ -0,0 +1,13 @@
+package dev.yerokha.flightstatusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightStatusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightStatusApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/yerokha/flightstatusapi/FlightStatusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yerokha.flightstatusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightStatusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
